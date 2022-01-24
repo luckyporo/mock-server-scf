04df3ce0cd796c24428c606b468bda69951788ab
luckyporo <565010808@qq.com>
2022-01-24 19:15:21 +0800

    update

--- src/app.js
+++ src/app.js
@@ -4,49 +4,64 @@ import Cos from 'cos-nodejs-sdk-v5'
 import { access, writeFile, readFile } from 'fs/promises'
 import chokidar from 'chokidar'
 
+const cos = new Cos({
+  SecretId: process.env.COS_SECRET_ID,
+  SecretKey: process.env.COS_SECRET_KEY,
+})
+
 ;(async () => {
-  const cos = new Cos({
-    SecretId: process.env.COS_SECRET_ID,
-    SecretKey: process.env.COS_SECRET_KEY,
-  })
-
-  const data = await cos.getObject({
-    Bucket: process.env.COS_BUCKET,
-    Region: process.env.COS_REGION,
-    Key: process.env.COS_DB_KEY,
-  })
-
   try {
     await access(process.env.TMP_DB_PATH)
   } catch (error) {
     console.error(error)
     if (error.code === 'ENOENT') {
-      await writeFile(process.env.TMP_DB_PATH, data.Body, 'utf8')
+      const dbData = await cos.getObject({
+        Bucket: process.env.COS_BUCKET,
+        Region: process.env.COS_REGION,
+        Key: process.env.COS_DB_KEY,
+      })
+      await writeFile(process.env.TMP_DB_PATH, dbData.Body, 'utf8')
+    } else {
+      throw new Error('读取db.json失败')
+    }
+  }
+
+  try {
+    await access(process.env.TMP_ROUTES_PATH)
+  } catch (error) {
+    console.error(error)
+    if (error.code === 'ENOENT') {
+      const routesData = await cos.getObject({
+        Bucket: process.env.COS_BUCKET,
+        Region: process.env.COS_REGION,
+        Key: process.env.COS_ROUTES_KEY,
+      })
+      await writeFile(process.env.TMP_ROUTES_PATH, routesData.Body, 'utf8')
+    } else {
+      throw new Error('读取routes.json失败')
     }
   }
 
   const server = JsonServer.create()
-  const router = JsonServer.router(process.env.TMP_DB_PATH)
-  const middlewares = JsonServer.defaults()
-  server.use(middlewares)
-  server.use(router)
+
+  server.use(JsonServer.rewriter(JSON.parse(await readFile(process.env.TMP_ROUTES_PATH, 'utf8'))))
+  server.use(JsonServer.router(process.env.TMP_DB_PATH))
+  server.use(JsonServer.defaults())
+
   server.listen(process.env.SERVER_PORT, () => {
     console.log('JSON Server is running')
   })
 })()
 
 chokidar.watch(process.env.TMP_DB_PATH).on('change', async () => {
-  const cos = new Cos({
-    SecretId: process.env.COS_SECRET_ID,
-    SecretKey: process.env.COS_SECRET_KEY,
-  })
-
-  const data = await cos.putObject({
-    Bucket: process.env.COS_BUCKET,
-    Region: process.env.COS_REGION,
-    Key: process.env.COS_DB_KEY,
-    Body: await readFile(process.env.TMP_DB_PATH, 'utf8'),
-  })
-
-  console.log(data)
+  try {
+    await cos.putObject({
+      Bucket: process.env.COS_BUCKET,
+      Region: process.env.COS_REGION,
+      Key: process.env.COS_DB_KEY,
+      Body: await readFile(process.env.TMP_DB_PATH, 'utf8'),
+    })
+  } catch (error) {
+    console.error(error)
+  }
 })
